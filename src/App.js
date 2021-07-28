dc07ea7709cf949fc621cc6a2cc0a0c8201eaf8b
Bhuswarna Kashyap <bhuswarnakashyap@gmail.com>
2021-07-29 00:19:06 +0530

    ThreeJS background added to front page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Cover from './components/cover_page';
 import Footer from './components/footer';
+import Background from './components/background';
 
 function App() {
   return (
     <div className="App">
-      <Cover />
+      <Background/>
+      <Cover/>
       <Footer/>
+      
     </div>
   );
 }
